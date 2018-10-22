76eb74efe69bd48974436c9b776ba26ae0ffaf53
Konstantin Barkalov <barkalov@vmk.unn.ru>
2018-10-22 19:31:08 +0300

    Compiled

--- src/tset.cpp
+++ src/tset.cpp
@@ -23,10 +23,15 @@ TSet::TSet(const TBitField &bf) : BitField(-1)
 
 TSet::operator TBitField()
 {
+  TBitField tbf(10);
+
+  return tbf;
+
 }
 
 int TSet::GetMaxPower(void) const // получить макс. к-во эл-тов
 {
+  return 0;
 }
 
 int TSet::IsMember(const int Elem) const // элемент множества?
@@ -46,6 +51,7 @@ void TSet::DelElem(const int Elem) // исключение элемента мн
 
 TSet& TSet::operator=(const TSet &s) // присваивание
 {
+  return *this;
 }
 
 int TSet::operator==(const TSet &s) const // сравнение
@@ -55,34 +61,45 @@ int TSet::operator==(const TSet &s) const // сравнение
 
 int TSet::operator!=(const TSet &s) const // сравнение
 {
+  return 0;
 }
 
 TSet TSet::operator+(const TSet &s) // объединение
 {
+  return s;
 }
 
 TSet TSet::operator+(const int Elem) // объединение с элементом
 {
+  TSet s(10);
+  return s;
 }
 
 TSet TSet::operator-(const int Elem) // разность с элементом
 {
+   TSet s(10);
+  return s;
 }
 
 TSet TSet::operator*(const TSet &s) // пересечение
 {
+  return s;
 }
 
 TSet TSet::operator~(void) // дополнение
 {
+   TSet s(10);
+  return s;
 }
 
 // перегрузка ввода/вывода
 
 istream &operator>>(istream &istr, TSet &s) // ввод
 {
+  return istr;
 }
 
 ostream& operator<<(ostream &ostr, const TSet &s) // вывод
 {
+  return ostr;
 }
